cbb18bce7aaa131807238a2e002b330717b70cdb
Dipti <diptijalgaonkar03@gmail.com>
2026-07-06 20:15:57 +0530

    added the register and login apis

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
+    path('register/', views.register_view, name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('products/', views.get_products),
     path('products/<int:pk>', views.get_product),
     path('categories/', views.get_categories),
